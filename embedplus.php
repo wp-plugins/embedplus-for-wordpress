bdb60f28f64676a5ea897d8444fe75cce13a8a3f
embedplus <embedplus@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-04-29 20:25:14 +0000

    git-svn-id: https://plugins.svn.wordpress.org/embedplus-for-wordpress/tags/1.2@378976 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- embedplus.php
+++ embedplus.php
@@ -72,7 +72,7 @@ function embedplusvideo_function($incomingfromhandler) {
     $epvars = preg_replace('/\s/', '', $epvars);
     $epstandard = preg_replace('/\s/', '', $epstandard);
 
-    $epstandard = preg_replace('/youtube.com\/v/i', 'youtube.com/embed/', $epstandard);
+    $epstandard = preg_replace('/youtube.com\/v\//i', 'youtube.com/embed/', $epstandard);
 
     if (preg_match('/youtube.com\/v/i', $epstandard)) {
         $epoutputstandard = '<object class="cantembedplus" height="~height" width="~width" type="application/x-shockwave-flash" data="~standard">' . chr(13) .
