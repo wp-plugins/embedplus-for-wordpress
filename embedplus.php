968265f84abbd679dcbf9f69c778a5765ecc7bb0
embedplus <embedplus@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-05-06 20:19:05 +0000

    git-svn-id: https://plugins.svn.wordpress.org/embedplus-for-wordpress/trunk@540574 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- embedplus.php
+++ embedplus.php
@@ -3,7 +3,7 @@
   Plugin Name: EmbedPlus for WordPress
   Plugin URI: http://www.embedplus.com
   Description: Enable WordPress to support enhanced EmbedPlus videos (slow motion, zoom, scene skipping, etc.)
-  Version: 2.0
+  Version: 2.1
   Author: EmbedPlus Team
   Author URI: http://www.embedplus.com
  */
