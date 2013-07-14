96d9e285fb1eb493f43c90266d84146b17da716b
embedplus <embedplus@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-07-14 01:44:46 +0000

    new version
    git-svn-id: https://plugins.svn.wordpress.org/embedplus-for-wordpress/tags/2.3.0@740432 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- js/embedplusstats_mce.js
+++ js/embedplusstats_mce.js
@@ -1,21 +1,15 @@
-<?php if (false)
-{
-    ?>
-    <script type="text/javascript">
-<?php } ?>
-
 (function() {    
     tinymce.create('tinymce.plugins.Embedplusstats', {
         init : function(ed, url) {
             var plep = new Image();
-            plep.src = url+'/btn_embedplusstats.png';
+            plep.src = url+'/../images/btn_embedplusstats.png';
             ed.addButton('embedplusstats', {
                 title : 'How much are your visitors actually watching the videos you post? Click here to start using this popular feature from EmbedPlus Labs »',
                 onclick : function(ev) {
-                    window.open('http://www.embedplus.com/dashboard/wordpress-video-analytics-seo.aspx', '_blank');
+                    window.open(epbasesite + '/dashboard/easy-video-analytics-seo.aspx?ref=wysiwygbutton&prokey=' + epprokey, '_blank');
                 }
             });
-                       
+
         },
         createControl : function(n, cm) {
             return null;
@@ -26,16 +20,10 @@
                 author : 'EmbedPlus',
                 authorurl : 'http://www.embedplus.com/',
                 infourl : 'http://www.embedplus.com/',
-                version : "2.6.0"
+                version : epversion
             };
         }
     });
     tinymce.PluginManager.add('embedplusstats', tinymce.plugins.Embedplusstats);
-    
-})();
 
-<?php if (false)
-{
-    ?>
-    </script>
-<?php } ?>
+})();
