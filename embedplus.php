953894d65be6aec1e5ed63a2ac702fdb35bdc0ee
embedplus <embedplus@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-20 00:54:26 +0000

    remove all prettyphoto references from all versions
    git-svn-id: https://plugins.svn.wordpress.org/embedplus-for-wordpress/tags/3.0@1163750 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- embedplus.php
+++ embedplus.php
@@ -748,9 +748,7 @@ class EmbedPlusOfficialPlugin
         </div>
         <script type="text/javascript">
             jQuery(document).ready(function($) {
-                                                                                                                                                                                                
-                $('.pp').prettyPhoto({ modal: false, theme: 'dark_rounded' });
-                                                                                                                                                                                		                                                                                                                 
+                                                                                                                                                                                                                                                                                                                                                                                		                                                                                                                 
                 jQuery('#prokeysubmit').click(function(){
                     jQuery(this).attr('disabled', 'disabled');
                     jQuery('#prokeyfailed').hide();
@@ -882,16 +880,12 @@ else
 {
     add_action('wp_print_scripts', 'embedplus_output_scriptvars');
     wp_enqueue_style('embedpluswiz', plugins_url() . '/embedplus-for-wordpress/css/embedplus_mce.css');
-    wp_enqueue_style('embedplusoptionscss', plugins_url() . '/embedplus-for-wordpress/css/prettyPhoto.css');
-    wp_enqueue_script('embedplusoptionsjs', plugins_url() . '/embedplus-for-wordpress/js/jquery.prettyPhoto.js');
 }
 
 function embedplus_admin_enqueue_scripts()
 {
     add_action('wp_print_scripts', 'embedplus_output_scriptvars');
     wp_enqueue_style('embedpluswiz', plugins_url() . '/embedplus-for-wordpress/css/embedplus_mce.css');
-    wp_enqueue_style('embedplusoptionscss', plugins_url() . '/embedplus-for-wordpress/css/prettyPhoto.css');
-    wp_enqueue_script('embedplusoptionsjs', plugins_url() . '/embedplus-for-wordpress/js/jquery.prettyPhoto.js');
 }
 
 function embedplus_output_scriptvars()
